5f05a9dea57e852c3eecbc8b4f2f144bd5edcfed
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-07 12:51:16 -0800

    Cleanup improvements

--- ports/neofetch/build.sh
+++ ports/neofetch/build.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/dylanaraps/neofetch
 cd neofetch
 sudo mkdir /usr/bob/neofetch
-sudo make DESTDIR=/usr/bob/neofetch install
-echo "alias neofetch='/usr/bob/neofetch/usr/bin/neofetch'" >> ~/.bashrc
+sudo make install
+echo "alias neofetch='/usr/bin/neofetch'" >> ~/.bashrc
+echo "\e[97m[*] \e[91mneofetch \e[37m- CLI Hardware Fetch tool written in bash" >> /usr/bin/ports/installed.sh
 cd .. && sudo rm -r neofetch
