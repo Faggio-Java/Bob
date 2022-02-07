f05e06262f467782828920cba1572e7fe7554f05
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-06 20:32:00 -0800

    Cleans up the installation a bit better

--- ports/neofetch/build.sh
+++ ports/neofetch/build.sh
@@ -2,5 +2,7 @@
 
 git clone https://github.com/dylanaraps/neofetch
 cd neofetch
-sudo make install
-sudo rm -r neofetch
+sudo mkdir /usr/bob/neofetch
+sudo make DESTDIR=/usr/bob/neofetch install
+echo "alias neofetch='/usr/bob/neofetch/usr/bin/neofetch'" >> ~/.bashrc
+cd .. && sudo rm -r neofetch
