674560684f048bbc42db2f3f651b914443ea5627
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-07 12:46:13 -0800

    Better Cleanup

--- ports/cmatrix/build.sh
+++ ports/cmatrix/build.sh
@@ -7,6 +7,7 @@ cd build
 cmake ..
 make
 sudo mkdir /usr/bob/cmatrix
-sudo make DESTDIR=/usr/bob/cmatrix install
-echo "alias cmatrix='/usr/bob/cmatrix/usr/local/bin/cmatrix'" >> ~/.bashrc
+sudo make install
+echo "alias cmatrix='/usr/local/bin/cmatrix'" >> ~/.bashrc
+echo "cMatrix" >> /usr/bin/ports/installed.sh
 cd ../../ && sudo rm -r cmatrix
