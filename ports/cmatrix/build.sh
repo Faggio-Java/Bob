d81da88287b466e890fb66650ecf630c6d54028e
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-06 20:32:48 -0800

    Fixed build errors & cleanup

--- ports/cmatrix/build.sh
+++ ports/cmatrix/build.sh
@@ -2,11 +2,11 @@
 
 git clone https://github.com/abishekvashok/cmatrix
 cd cmatrix
-
 mkdir -p build
 cd build
 cmake ..
 make
-sudo make install
-echo "alias cmatrix='/usr/local/bin/cmatrix'" >> ~/.bashrc
-sudo rm -r cmatrix
+sudo mkdir /usr/bob/cmatrix
+sudo make DESTDIR=/usr/bob/cmatrix install
+echo "alias cmatrix='/usr/bob/cmatrix/usr/local/bin/cmatrix'" >> ~/.bashrc
+cd ../../ && sudo rm -r cmatrix
