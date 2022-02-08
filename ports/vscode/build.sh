cb5865873bee06448f4037eb7399d1109515dc90
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-08 03:33:02 -0600

    Added package description

--- ports/vscode/build.sh
+++ ports/vscode/build.sh
@@ -4,3 +4,5 @@ sudo corepack enable
 git clone https://github.com/microsoft/vscode
 cd vscode
 yarn
+echo "\e[97m[*] \e[91mcode-oss \e[37m- Microsoft's open source ide" >> /usr/bin/ports/installed.sh
+cd .. && sudo rm -r vscode
