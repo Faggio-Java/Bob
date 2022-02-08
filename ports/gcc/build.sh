2291afd760c65fa43648b6afc98da8064b213fa5
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-08 03:37:41 -0600

    Added package description

--- ports/gcc/build.sh
+++ ports/gcc/build.sh
@@ -1,5 +1,7 @@
 #!bin/bash
-git clone https://github.com/gcc-mirror/gcc.git
+
+sudo git clone https://github.com/gcc-mirror/gcc.git
+cd gcc
 ./contrib/download_prerequisites
 cd ..
 mkdir objdir
@@ -7,3 +9,4 @@ cd objdir
 $PWD/../gcc-4.6.2/configure --prefix=$HOME/GCC-4.6.2 --enable-languages=c,c++,fortran,go
 make
 sudo make install
+echo "\e[97m[*] \e[91mgcc \e[37m- The GNU compiler collection, which includes C and C++ compilers" >> /usr/bin/ports/installed.sh
